a5f9938ceaa0410d078047b4bda3990a7a630cfa
RSANJAYKUMAR <47408335+RSANJAYKUMAR@users.noreply.github.com>
2019-02-08 12:04:05 +0530

    Update and rename palinooo.py to pali.py

--- pali.py
+++ pali.py
@@ -1,10 +1,10 @@
-n=int(input())
-temp=n
+s=int(input())
+temp=s
 rev=0
-while(n>0):
-  dig=n%10
+while(s>0):
+  dig=s%10
   rev=rev*10+dig
-  n=n//10
+  s=s//10
 if(temp==rev):
 	print("yes")
 else:
